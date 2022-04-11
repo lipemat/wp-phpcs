1faf90dc5d3db6a23232f011549a8aa9e42086c9
lipemat <mat@matlipe.com>
2022-04-11 14:41:46 -0400

    Remove custom logic from StringConcatSniff
    The VIP StringConcatSniff checks for string concatenation and will pass if any function wraps the string.
    
    No need to check for our custom sanitize.

--- src/Lipe/Sniffs/JS/StringConcatSniff.php
+++ src/Lipe/Sniffs/JS/StringConcatSniff.php
@@ -12,36 +12,11 @@ use PHP_CodeSniffer\Util\Tokens;
 
 /**
  * Looks for HTML string concatenation.
+ *
+ * Using VIP sniff verbatim as the sniff does not care what
+ * function is used to escape, just that a function is used.
+ *
+ * Here to keep the `Lipe.JS`sniffs fully inclusive.
  */
 class StringConcatSniff extends \WordPressVIPMinimum\Sniffs\JS\StringConcatSniff {
-
-	use EscapeOutputFunctions;
-
-	/**
-	 * Processes this test, when one of its tokens is encountered.
-	 *
-	 * @param integer $stackPtr The position of the current token in the stack passed in $tokens.
-	 *
-	 * @return void
-	 */
-	public function process_token( $stackPtr ) {
-		$nextToken = $this->phpcsFile->findNext( Tokens::$emptyTokens, ( $stackPtr + 1 ), null, true, null, true );
-
-		if ( $this->tokens[ $nextToken ]['code'] === T_CONSTANT_ENCAPSED_STRING && strpos( $this->tokens[ $nextToken ]['content'], '<' ) !== false && preg_match( '/\<\/[a-zA-Z]+/', $this->tokens[ $nextToken ]['content'] ) === 1 ) {
-			if ( $this->isEscapeFunction( $nextToken ) ) {
-				// It's a custom scape function.
-				return;
-			}
-		}
-
-		$prevToken = $this->phpcsFile->findPrevious( Tokens::$emptyTokens, ( $stackPtr - 1 ), null, true, null, true );
-
-		if ( $this->tokens[ $prevToken ]['code'] === T_CONSTANT_ENCAPSED_STRING && strpos( $this->tokens[ $prevToken ]['content'], '<' ) !== false && preg_match( '/\<[a-zA-Z]+/', $this->tokens[ $prevToken ]['content'] ) === 1 ) {
-			if ( $this->isEscapeFunction( $prevToken ) ) {
-				return;
-			}
-		}
-
-		parent::process_token( $stackPtr );
-	}
 }
