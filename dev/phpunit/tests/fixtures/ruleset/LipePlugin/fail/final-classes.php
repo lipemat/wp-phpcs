8e2a05fa1e1800365a399227c73966c4aadbe12c
lipemat <mat@matlipe.com>
2023-08-09 16:18:18 -0500

    Support testing the LipePlugin ruleset with fixtures

---
+++ dev/phpunit/tests/fixtures/ruleset/LipePlugin/fail/final-classes.php
@@ -0,0 +1,23 @@
+<?php
+
+final class Nope {
+
+}
+
+/**
+ * A class with doc comments which extends.
+ *
+ * @author Mat Lipe
+ * @since  August 2023
+ */
+final class NopeExtends extends FixtureTests {
+
+}
+
+/**
+ * @author Mat Lipe
+ * @since  August 2023
+ */
+class NotFinal {
+
+}
