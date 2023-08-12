3ed5454416cfffd56738cd78142f4d1145c047e6
lipemat <mat@matlipe.com>
2023-08-12 15:48:35 -0500

    Create unit tests for PrivateInClassSniff

---
+++ dev/phpunit/fixtures/ruleset/LipePlugin/fail/private-in-class.php
@@ -0,0 +1,64 @@
+<?php
+
+class Nope {
+	private static $stuck = 'foo';
+
+
+	public function get_stuck() : string {
+		return self::$stuck;
+	}
+
+
+	private static function get_stuck_static() : string {
+		return self::$stuck;
+	}
+
+
+	public function call() : string {
+		return self::get_stuck_static();
+	}
+}
+
+/**
+ * Some comments.
+ *
+ * @notice Constants in a final class cannot be static.
+ */
+final class NopeFinal {
+	/**
+	 * Some comments.
+	 *
+	 * @notice Constants in a final class cannot be static.
+	 */
+	private static $stuck = 'foo';
+
+
+	/**
+	 * Get stuck.
+	 *
+	 * @return string
+	 */
+	public function get_stuck() : string {
+		return self::$stuck;
+	}
+
+
+	/**
+	 * Get stuck static.
+	 *
+	 * @return string
+	 */
+	private static function get_stuck_static() : string {
+		return self::$stuck;
+	}
+
+
+	/**
+	 * Call.
+	 *
+	 * @return string
+	 */
+	public function call() : string {
+		return self::get_stuck_static();
+	}
+}
---
+++ dev/phpunit/fixtures/ruleset/LipePlugin/pass/private-in-class.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Noop functions for load-scripts.php and load-styles.php.
+ *
+ * @since      4.4.0
+ * @subpackage Administration
+ * @package    WordPress
+ */
+
+/**
+ * Noop functions for load-scripts.php and load-styles.php.
+ *
+ * @since      4.4.0
+ * @subpackage Administration
+ * @package    WordPress
+ */
+class Yep {
+	/**
+	 * Stuck.
+	 *
+	 * @var string
+	 */
+	protected static $stuck = 'foo';
+
+
+	/**
+	 * Get stuck.
+	 *
+	 * @return string
+	 */
+	public function get_stuck() : string {
+		return static::$stuck;
+	}
+}
