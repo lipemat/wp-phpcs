0b8007df8f2468b1decc2f0a178bb815f9e189c5
lipemat <mat@matlipe.com>
2023-08-31 13:41:37 -0400

    Introduce Lipe.PHP.DisallowNullCoalesceInForLoops sniff
    - Prevent null coalesce in foreach loops.
    - Prevent null coalesce in for loops.
    - Prevent ternary in foreach loops.
    - Prevent ternary in for loops.

---
+++ dev/phpunit/fixtures/pass/disallow-null-coalesce-in-for-loops.php
@@ -0,0 +1,25 @@
+<?php
+/** @noinspection ALL */
+foreach ( $attributes['properties'] as $property => $value ) {
+		$attributes['properties'] ?? [];
+}
+
+foreach ( $attributes['properties'] as $property => $value ) {
+	$attributes['properties'] ??= [];
+}
+
+for ( $i = 1; $i < $attributes['properties']; $i ++ ) {
+		$attributes['properties'] ?? 4;
+}
+
+for ( $i = 1; $i < $attributes['properties']; $i ++ ) {
+	$attributes['properties'] ??= 4;
+}
+
+foreach ( $attributes['properties'] as $property => $value ) {
+	$attributes['properties'] ? [] : [ 1 ];
+}
+
+for ( $i = 1; $i < $attributes['properties']; $i ++ ) {
+	$attributes['properties'] ? 4 : 3;
+}
